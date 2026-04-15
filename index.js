e7f2fe892c1a495831a60fc30d55e2816e2d2eb4
Nina Adora <nirinakaes@gmail.com>
2026-04-15 14:25:37 +0300

    fix: update weather API call and improve alert display functionality

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
 // index.js
-const weatherApi = "https://api.weather.gov/alerts/active?area="
-
+const weatherApi = "https://api.weather.gov/alerts/active?area=ohio"
 // Your code here!
+
+const state= document.querySelector('#state-input')
+
+async function fetchWeatherAlerts (state){ try
+    {
+    const response = await fetch(weatherApi)
+
+    const data = await response.json()
+    
+
+    catch(error){
+        console.log (error)
+    }
+}
+}
+
+displayAlerts(data)
+
+ const alertsDisplay = document.querySelector('#alerts-display')
+ const fetchAlert = document.querySelector('#fetch-alerts')
+ const errorMessage = document.querySelector('#error-message')
+
+ fetchAlert.addEventListener('click', (displayAlerts)=> {
+    displayAlerts.preventdefault()
+ })
+
+function displayAlerts(data){
+    data.features.forEach(element => {
+        
+        const li = document.createElement('li')
+
+        li.textContent = element.properties.headline
+
+        alertsDisplay.appendChild(li)
+         
+        
+    });
+    
+}
